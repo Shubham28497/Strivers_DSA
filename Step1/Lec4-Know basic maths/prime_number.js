1354bbc34cde55ed03aed8700a8ee1fa3da32b35
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-04-26 12:16:46 +0530

    add logic

--- Step1/Lec4-Know basic maths/prime_number.js
+++ Step1/Lec4-Know basic maths/prime_number.js
@@ -4,4 +4,14 @@
 
 // Explanation: The only divisors of 5 are 1 and 5 , So the number 5 is prime.
 
+var isPrime = function(n) {
+    if (n <= 1) return false;          // 0 and 1 are not prime
 
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false; // Found a divisor other than 1 and n
+    }
+
+    return true;
+};
+
+console.log(isPrime(5));  // Output: true
