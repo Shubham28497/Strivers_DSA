79257fb74f75d7988bc1a38c4d87c6fbf7a4f739
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-04-26 11:56:52 +0530

    add logic

--- Step1/Lec4-Know basic maths/print_all_divisor.js
+++ Step1/Lec4-Know basic maths/print_all_divisor.js
@@ -3,3 +3,12 @@
 // Output = [1, 2, 3, 6]
 
 // Explanation: The divisors of 6 are 1, 2, 3, 6.
+var printDiv= function(x){
+    for(let i=1;i<=x;i++){
+        if(x%i===0){
+        console.log(i)
+        }
+    }
+ 
+}
+printDiv(36)
