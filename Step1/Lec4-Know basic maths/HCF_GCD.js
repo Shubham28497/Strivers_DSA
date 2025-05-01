cbfa4dd5e275a7e4ffb5f4a9fcf45d72900dde98
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-01 11:12:12 +0530

    add brute force logic

--- Step1/Lec4-Know basic maths/HCF_GCD.js
+++ Step1/Lec4-Know basic maths/HCF_GCD.js
@@ -5,3 +5,19 @@
 // Explanation: Divisors of n1 = 1, 2, 4, Divisors of n2 = 1, 2, 3, 6
 
 // Greatest Common divisor = 2.
+
+//Brute Force
+var findGCD = function(num1,num2){
+    let gcd=1;
+    let min=Math.min(num1,num2)
+    for(let i=0;i<=min;i++){
+        if(num1%i==0 && num2%i==0){
+            gcd=i;
+        }
+    }
+    return gcd;
+}
+let num1 = 10;
+let num2 = 5;
+
+console.log("HCF/GCD is:", findGCD(num1, num2));
