51b26ebac136cdc10b0f723b22639369e0adafb0
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-01 13:32:01 +0530

    Euclidean Algorithm

--- Step1/Lec4-Know basic maths/HCF_GCD.js
+++ Step1/Lec4-Know basic maths/HCF_GCD.js
@@ -23,3 +23,10 @@ let num2 = 5;
 console.log("HCF/GCD is:", findGCD(num1, num2)); 
 
 //TC : O(min(a, b))
+
+// What is the Euclidean Algorithm?
+// It’s a method for finding the Greatest Common Divisor (GCD) of two numbers based on this idea:
+
+// GCD(a, b) = GCD(b, a % b)
+// Repeat this until b = 0. The result is a.
+
