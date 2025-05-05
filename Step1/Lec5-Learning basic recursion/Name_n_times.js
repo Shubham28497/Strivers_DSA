573dd992cace6b96aec40ebd7ec4513797d0afaf
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-05 11:53:26 +0530

    print name n of times using recursion

--- Step1/Lec5-Learning basic recursion/Name_n_times.js
+++ Step1/Lec5-Learning basic recursion/Name_n_times.js
@@ -1,1 +1,13 @@
 // Problem: Print your Name N times using recursion
+
+function printNnames(name,n){
+    function helper(i){
+        if(i>n)
+            return;
+        console.log(name)
+        helper(i+1);
+    }
+    helper(1)
+}
+
+printNnames("Shubh",5)
