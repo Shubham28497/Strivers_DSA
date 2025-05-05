2a44fb726ac0a706da1ec01ff26c1ec3b3b645a1
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-05 12:08:28 +0530

    add sol

--- Step1/Lec5-Learning basic recursion/1_to_N.js
+++ Step1/Lec5-Learning basic recursion/1_to_N.js
@@ -1,1 +1,12 @@
 // Problem: Print from 1 to N using Recursion
+
+function print1toN(n){
+function helper(i){
+    if(i>n)
+        return;
+    console.log(i)
+    helper(i+1)
+}
+    helper(1)
+}
+print1toN(4)
