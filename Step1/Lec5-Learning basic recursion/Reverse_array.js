6f6d51a8d17c91b60610aeb126578f515d78889d
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-06 12:44:15 +0530

    2nd logic with one index

--- Step1/Lec5-Learning basic recursion/Reverse_array.js
+++ Step1/Lec5-Learning basic recursion/Reverse_array.js
@@ -10,3 +10,14 @@ function revArray(arr,start=0,end=arr.length-1){
 let arr = [1, 2, 3, 4, 5];
 revArray(arr);
 console.log(arr)
+
+//Using one index
+function arrayRev(arr2,i=0){
+    let n=arr2.length
+    if(i>=Math.floor(n/2)) return;
+    [arr2[i],arr2[n-1-i]]=[arr2[n-i-1],arr2[i]]
+    arrayRev(arr2,i+1)
+    }
+    const arr2=[1,2,3,4,5]
+    arrayRev(arr2)
+    console.log(arr2)
