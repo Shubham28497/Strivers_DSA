c7ee66926e11ad922134882fb54e7ae934b9189e
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-06 13:50:28 +0530

    add qstn

---
+++ Step1/Lec5-Learning basic recursion/check_string_palindrome.js
@@ -0,0 +1,6 @@
+// Input : s = "hannah"
+// Output : true
+// Explanation : The string when reversed is --> "hannah", which is same as original string , so we return true.
+// Input : s = "aabbaaa"
+// Output : false
+// Explanation : The string when reversed is --> "aaabbaa", which is not same as original string, So we return false.
