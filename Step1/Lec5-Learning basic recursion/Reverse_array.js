b56f0c96d0f57651e4d579fa7e225a1af1f6422f
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-06 11:21:09 +0530

    add logic to solve revArray

--- Step1/Lec5-Learning basic recursion/Reverse_array.js
+++ Step1/Lec5-Learning basic recursion/Reverse_array.js
@@ -1,9 +1,12 @@
 // Input: n=5, arr = [1,2,3,4,5]
-
-
-
 // Output: [5,4,3,2,1]
-
-
-
 // Explanation: The reverse of the array [1,2,3,4,5] is [5,4,3,2,1]
+function revArray(arr,start=0,end=arr.length-1){
+    if(start>=end) return;{
+        [arr[start],arr[end]]=[arr[end],arr[start]]
+        revArray(arr,start+1,end-1)
+    }
+}
+let arr = [1, 2, 3, 4, 5];
+revArray(arr);
+console.log(arr)
