36a63cdca0334977037d696acd3dd60d8a89fa38
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-06 14:07:27 +0530

    add logic

--- Step1/Lec5-Learning basic recursion/check_string_palindrome.js
+++ Step1/Lec5-Learning basic recursion/check_string_palindrome.js
@@ -4,3 +4,12 @@
 // Input : s = "aabbaaa"
 // Output : false
 // Explanation : The string when reversed is --> "aaabbaa", which is not same as original string, So we return false.
+function checkPalindromeString(str,i=0){
+    n=str.length;
+if(i>=Math.floor(n/2)) return true;
+if(str[i]!==str[n-i-1]) return false;
+return checkPalindromeString(str,i+1)
+}
+console.log(checkPalindromeString("madam"));  // true
+console.log(checkPalindromeString("hello"));  // false
+
