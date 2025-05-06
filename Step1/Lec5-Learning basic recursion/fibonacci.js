0cc493ba760f8a08d8dfdd8c8b25af01819b5584
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-06 22:04:10 +0530

    add sol

--- Step1/Lec5-Learning basic recursion/fibonacci.js
+++ Step1/Lec5-Learning basic recursion/fibonacci.js
@@ -10,3 +10,12 @@
 // Output : 2
 
 // Explanation : F(3) = F(2) + F(1) => 1 + 1 => 2.
+function fibonacci(n) {
+    if (n <= 1) return n;                   // Base case
+    return fibonacci(n - 1) + fibonacci(n - 2); // Recursive step
+  }
+  
+  // Example
+  console.log(fibonacci(5));  // Output: 5
+  console.log(fibonacci(7));  // Output: 13
+  
