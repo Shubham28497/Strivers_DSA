905cb8893f90d56e5abdfa0bf08f8d278d719ce7
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-15 10:18:18 +0530

    logic of selectionSort

---
+++ Step2-Learning-sorting-techniques/Lec1-Sorting 1/selection_sort.js
@@ -0,0 +1,19 @@
+//Select minimum and swap it
+function selectionSort(arr){
+let n=arr.length;
+for(let i=0;i<n-1;i++){
+    let minIndex=i;
+    for(let j=i+1;j<n;j++){
+        if(arr[j]<arr[minIndex]){
+            minIndex=j
+        }
+    }
+    if(minIndex!==i){
+        [arr[i],arr[minIndex]]=[arr[minIndex],arr[i]]
+    }
+}
+return arr;
+
+}
+let arr=[64, 25, 12, 22, 11];
+console.log(selectionSort(arr))
