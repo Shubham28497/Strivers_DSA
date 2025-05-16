5b9ae007c8a3e7998c7abf4c018161ee56be396f
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-16 12:34:58 +0530

    add logic of insertion sort

--- Step2-Learning-sorting-techniques/Lec1-Sorting 1/insertion_sort.js
+++ Step2-Learning-sorting-techniques/Lec1-Sorting 1/insertion_sort.js
@@ -1,1 +1,17 @@
-//Take an element and place it in its current order
+//Take an element and place it in its correct order
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      let key = arr[i];
+      let j = i - 1;
+  
+      while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j]; // Shift right
+        j--;
+      }
+  
+      arr[j + 1] = key; // Insert key in correct place
+    }
+    return arr;
+  }
+  arr = [8, 4, 1, 5, 2]
+  console.log(insertionSort(arr))
