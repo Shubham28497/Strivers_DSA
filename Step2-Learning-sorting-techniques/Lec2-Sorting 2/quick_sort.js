77f62aa73db67156222aeddb1b9b2024ff00de38
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-19 14:08:32 +0530

    add logic of quick sort

--- Step2-Learning-sorting-techniques/Lec2-Sorting 2/quick_sort.js
+++ Step2-Learning-sorting-techniques/Lec2-Sorting 2/quick_sort.js
@@ -12,3 +12,20 @@
 // Output: 1 2 3 4 5 6 7 9
 // Explanation: After sorting the array becomes 1, 3, 4, 7, 9
 
+function quickSort(arr){
+    if (arr.length <= 1) return arr;
+let pivot=arr[0];
+let left=[]
+let right=[]
+for(let i=1;i<arr.length;i++){
+   if(arr[i]<pivot){
+       left.push(arr[i])
+   }
+   else{
+       right.push(arr[i])
+   }
+}
+return [...quickSort(left), pivot, ...quickSort(right)];
+}
+let arr=[8,7,6,3,4,5,2,1]
+console.log(quickSort(arr))
