96bbbc1ee0f5f220cfebb79cbb39dcb772536e2f
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-19 16:53:43 +0530

    add tc and sc

--- Step3-Solve-problems-on-array/Lec1-Easy/array_sorted_or_not.js
+++ Step3-Solve-problems-on-array/Lec1-Easy/array_sorted_or_not.js
@@ -11,3 +11,5 @@ console.log(isSorted([1, 2, 2, 3]));        // true
 console.log(isSorted([5, 3, 1]));           // false
 console.log(isSorted([10]));               // true (single element)
 console.log(isSorted([]));                 // true (empty array is trivially sorted)
+//TC: O(n)
+//SC: O(1)
