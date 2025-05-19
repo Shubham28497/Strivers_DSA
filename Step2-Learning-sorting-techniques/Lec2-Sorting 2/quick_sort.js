cbcc404f81408f98984461d46fe2e00cb942a37e
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-19 13:47:44 +0530

    quick sort

---
+++ Step2-Learning-sorting-techniques/Lec2-Sorting 2/quick_sort.js
@@ -0,0 +1,16 @@
+//Pick the pivot and place it in its correct place in the sorted array
+//pivot el can be 1st last mid and any random element
+// Problem Statement:  Given an array of n integers, sort the array using the Quicksort method.
+
+// Examples:
+
+// Example 1:
+// Input:  N = 5  , Arr[] = {4,1,7,9,3}
+// Output: 1 3 4 7 9 
+
+// Explanation: After sorting the array becomes 1, 3, 4, 7, 9
+
+// Example 2:
+// Input: N = 8 , Arr[] = {4,6,2,5,7,9,1,3}
+// Output: 1 2 3 4 5 6 7 9
+// Explanation: After sorting the array becomes 1, 3, 4, 7, 9
