7498bd63056f9bcd9e4c84f78836e59f4c5af5d6
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-19 15:18:49 +0530

    add brute force for largest element in array

--- Step3-Solve-problems-on-array/Lec1-Easy/largest_el_array.js
+++ Step3-Solve-problems-on-array/Lec1-Easy/largest_el_array.js
@@ -4,3 +4,15 @@
 // Input: nums = [3, 3, 0, 99, -40]
 // Output: 99
 // Explanation: The largest element in array is 99
+//1.brute force
+function largestElement(arr){
+    let max=arr[0]
+for(let i=1;i<arr.length;i++){
+if(arr[i]>max){
+max=arr[i]
+}
+}
+return max
+}
+let arr=[3, 3, 0, 99, -40]
+console.log(largestElement(arr))
