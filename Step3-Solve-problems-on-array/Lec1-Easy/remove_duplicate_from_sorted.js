af63705b78323c64f8ad2f3f3f1e0ee289f7296c
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-19 17:39:49 +0530

    add sol

--- Step3-Solve-problems-on-array/Lec1-Easy/remove_duplicate_from_sorted.js
+++ Step3-Solve-problems-on-array/Lec1-Easy/remove_duplicate_from_sorted.js
@@ -2,3 +2,15 @@
 // Output: 4
 // Explanation: Resulting array = [0, 3, 5, 6, _, _]
 // There are 4 distinct elements in nums and the elements marked as _ can have any value.
+function removeDuplicates(arr) {
+    let i=0;
+    for(let j=1;j<arr.length;j++){
+        if(arr[i]!==arr[j]){
+            arr[i+1]=arr[j]
+            i++;
+        }
+    }
+    return i+1
+   }
+   const arr = [1,1,2,2,2,3,3];
+   console.log(removeDuplicates(arr))
