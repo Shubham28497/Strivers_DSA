28a51679481157a11e598b50de05420fec7f872a
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-19 15:04:51 +0530

    largest element in array example

---
+++ Step3-Solve-problems-on-array/Lec1-Easy/largest_el_array.js
@@ -0,0 +1,6 @@
+// Input: nums = [3, 3, 6, 1]
+// Output: 6
+// Explanation: The largest element in array is 6
+// Input: nums = [3, 3, 0, 99, -40]
+// Output: 99
+// Explanation: The largest element in array is 99
