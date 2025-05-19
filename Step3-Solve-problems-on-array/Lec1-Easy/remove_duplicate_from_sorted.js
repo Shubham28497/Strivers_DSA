b45d50e4b9fdad8bdb6008c339514d961dc01272
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-19 17:39:04 +0530

    add qstn and ex of remove duplicate from sorted array

---
+++ Step3-Solve-problems-on-array/Lec1-Easy/remove_duplicate_from_sorted.js
@@ -0,0 +1,4 @@
+// Input: nums = [0, 0, 3, 3, 5, 6]
+// Output: 4
+// Explanation: Resulting array = [0, 3, 5, 6, _, _]
+// There are 4 distinct elements in nums and the elements marked as _ can have any value.
