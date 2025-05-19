4c96310a1830700fe608e149e37a68aaf98b2af0
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-19 16:49:41 +0530

    add func and console

---
+++ Step3-Solve-problems-on-array/Lec1-Easy/array_sorted_or_not.js
@@ -0,0 +1,8 @@
+function isSorted(arr){
+
+}
+console.log(isSorted([1, 2, 3, 4, 5]));     // true
+console.log(isSorted([1, 2, 2, 3]));        // true
+console.log(isSorted([5, 3, 1]));           // false
+console.log(isSorted([10]));               // true (single element)
+console.log(isSorted([]));                 // true (empty array is trivially sorted)
