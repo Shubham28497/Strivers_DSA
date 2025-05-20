003ded4cc38adfad9e972612b533858b09a47caf
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-20 12:33:28 +0530

    add logic

--- Step3-Solve-problems-on-array/Lec1-Easy/moves_0_end.js
+++ Step3-Solve-problems-on-array/Lec1-Easy/moves_0_end.js
@@ -5,3 +5,19 @@
 // Input: nums = [0, 0, 0, 1, 3, -2]
 // Output: [1, 3, -2, 0, 0, 0]
 // Explanation: All 3 zeroes are moved to the end and the order of the other elements stay the same
+function moveZerosToEnd(arr){
+    let j=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]!==0){
+            arr[j]=arr[i]
+            j++
+        }
+    }
+    while(j<arr.length){
+        arr[j]=0;
+        j++
+    }
+    return arr
+    }
+    let arr = [0, 1, 0, 3, 12]
+    console.log(moveZerosToEnd(arr))
