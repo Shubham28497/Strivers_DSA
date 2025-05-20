52bbe980c9a74fcb6c470c52b9220010ff87a8f6
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-20 15:37:53 +0530

    add logic

--- Step3-Solve-problems-on-array/Lec1-Easy/missing_no_array.js
+++ Step3-Solve-problems-on-array/Lec1-Easy/missing_no_array.js
@@ -4,3 +4,16 @@
 // Input: nums = [0, 1, 2, 4, 5, 6]
 // Output: 3
 // Explanation: nums contains 0, 1, 2, 4, 5, 6 thus leaving 3 as the only missing number in the range [0, 6]
+function findMissingNumber(arr){
+    let n=arr.length+1
+    let expectedsum=(n*(n+1)/2)
+    let actualSum=0;
+  for(let i=0;i<arr.length;i++){
+      actualSum=actualSum+arr[i]
+  }
+  return expectedsum - actualSum
+ }
+ 
+ 
+ let arr = [1,2,3,4,5,7]
+ console.log(findMissingNumber(arr))
