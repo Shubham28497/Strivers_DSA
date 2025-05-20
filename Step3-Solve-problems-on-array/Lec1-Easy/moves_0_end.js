3854ecaa6685c173f352481cb8a0f4b85521c3b9
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-20 12:12:28 +0530

    add qstn and example of moves 0 to an end

---
+++ Step3-Solve-problems-on-array/Lec1-Easy/moves_0_end.js
@@ -0,0 +1,7 @@
+// Examples:
+// Input: nums = [0, 1, 4, 0, 5, 2]
+// Output: [1, 4, 5, 2, 0, 0]
+// Explanation: Both the zeroes are moved to the end and the order of the other elements stay the same
+// Input: nums = [0, 0, 0, 1, 3, -2]
+// Output: [1, 3, -2, 0, 0, 0]
+// Explanation: All 3 zeroes are moved to the end and the order of the other elements stay the same
