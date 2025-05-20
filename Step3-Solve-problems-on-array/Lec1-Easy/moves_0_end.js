92fd4efce4ae3d05cf1cb6126dd77f0bd58418d7
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-20 12:34:28 +0530

    add step by step execution of 1 loop

--- Step3-Solve-problems-on-array/Lec1-Easy/moves_0_end.js
+++ Step3-Solve-problems-on-array/Lec1-Easy/moves_0_end.js
@@ -13,6 +13,37 @@ function moveZerosToEnd(arr){
             j++
         }
     }
+    //Step by step execution of loop1st
+    // i = 0 → arr[0] = 0
+    // arr[0] is 0 → skip
+    
+    // j = 0
+    
+    // ➤ i = 1 → arr[1] = 1
+    // arr[1] is not 0 → copy arr[1] to arr[0]
+    
+    // arr becomes: [1, 1, 0, 3, 12]
+    
+    // j = 1
+    
+    // ➤ i = 2 → arr[2] = 0
+    // arr[2] is 0 → skip
+    
+    // j = 1
+    
+    // ➤ i = 3 → arr[3] = 3
+    // arr[3] is not 0 → copy arr[3] to arr[1]
+    
+    // arr becomes: [1, 3, 0, 3, 12]
+    
+    // j = 2
+    
+    // ➤ i = 4 → arr[4] = 12
+    // arr[4] is not 0 → copy arr[4] to arr[2]
+    
+    // arr becomes: [1, 3, 12, 3, 12]
+    
+    // j = 3
     while(j<arr.length){
         arr[j]=0;
         j++
