1f0d04f2b6543dd6557d3665b4fe4ed054924760
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-20 15:14:09 +0530

    add logic

--- Step3-Solve-problems-on-array/Lec1-Easy/linear_search.js
+++ Step3-Solve-problems-on-array/Lec1-Easy/linear_search.js
@@ -4,3 +4,16 @@
 // Input: nums = [2, -4, 4, 0, 10], target = 6
 // Output: -1
 // Explanation: The value 6 does not occur in the array, hence output is -1
+function linearSearch(arr,target){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]==target){
+            return i;
+        }
+    }
+    return -1
+}
+
+
+let arr = [2, 3, 4, 5, 3];
+let target = 3;
+console.log(linearSearch(arr, target));
