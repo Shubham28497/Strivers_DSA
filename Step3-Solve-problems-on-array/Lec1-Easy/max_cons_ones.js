346422eec76acaa5f195d5cd349b53148ae5fdef
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-20 17:22:15 +0530

    add sol

--- Step3-Solve-problems-on-array/Lec1-Easy/max_cons_ones.js
+++ Step3-Solve-problems-on-array/Lec1-Easy/max_cons_ones.js
@@ -5,3 +5,22 @@
 // Input: nums = [0, 0, 0, 0, 0, 0, 0, 0]
 // Output: 0
 // Explanation: No 1s are present in nums, thus we return 0
+function findMaxConsecutiveOnes(arr){
+    let cur=0;
+    let maxCount=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]===1){
+            cur++;
+        if(cur>maxCount){
+            maxCount=cur;
+        }
+        }
+        else{
+            cur=0;
+        }
+    }
+    return maxCount;
+  }
+  let arr = [1, 1, 0, 1, 1, 1]
+  console.log(findMaxConsecutiveOnes(arr))
+  
