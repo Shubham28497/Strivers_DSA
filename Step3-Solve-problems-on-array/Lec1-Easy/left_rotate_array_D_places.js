706a93c18ff98d54948dba94f242bc067b2cf969
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-20 10:03:35 +0530

    add qstns and example

---
+++ Step3-Solve-problems-on-array/Lec1-Easy/left_rotate_array_D_places.js
@@ -0,0 +1,5 @@
+// Examples:
+// Input: nums = [1, 2, 3, 4, 5, 6], k = 2
+// Output: nums = [3, 4, 5, 6, 1, 2]
+// Explanation: rotate 1 step to the left: [2, 3, 4, 5, 6, 1]
+// rotate 2 steps to the left: [3, 4, 5, 6, 1, 2]
