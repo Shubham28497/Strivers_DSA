7044a8c53e8b2261552248eca8317a085866b218
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-20 15:46:26 +0530

    add qstn and ex

---
+++ Step3-Solve-problems-on-array/Lec1-Easy/max_cons_ones.js
@@ -0,0 +1,7 @@
+// Examples:
+// Input: nums = [1, 1, 0, 0, 1, 1, 1, 0]
+// Output: 3
+// Explanation: The maximum consecutive 1s are present from index 4 to index 6, amounting to 3 1s
+// Input: nums = [0, 0, 0, 0, 0, 0, 0, 0]
+// Output: 0
+// Explanation: No 1s are present in nums, thus we return 0
