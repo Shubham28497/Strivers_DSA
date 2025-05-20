b8521d0b4808dd8e176ba8e8d0c51921904636f3
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-20 11:15:05 +0530

    add brute force logic

--- Step3-Solve-problems-on-array/Lec1-Easy/left_rotate_array_D_places.js
+++ Step3-Solve-problems-on-array/Lec1-Easy/left_rotate_array_D_places.js
@@ -3,3 +3,23 @@
 // Output: nums = [3, 4, 5, 6, 1, 2]
 // Explanation: rotate 1 step to the left: [2, 3, 4, 5, 6, 1]
 // rotate 2 steps to the left: [3, 4, 5, 6, 1, 2]
+
+// 1.Brute Force Approach
+function leftRotateByOne(arr) {
+    let temp=arr[0]
+    for(let i=1;i<arr.length;i++){
+        arr[i-1]=arr[i]
+    }
+    arr[arr.length-1]=temp
+    }
+    
+    function leftRotateBrute(arr, d) {
+      for (let i = 0; i < d; i++) {
+        leftRotateByOne(arr);
+      }
+      return arr;
+    }
+    
+    let arr = [1, 2, 3, 4, 5]
+    let D = 2 
+    console.log(leftRotateBrute(arr,D))
