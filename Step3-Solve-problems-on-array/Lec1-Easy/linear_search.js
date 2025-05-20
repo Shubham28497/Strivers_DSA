9a24e0755190282d07020449a581ce1c763188ec
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-20 15:09:11 +0530

    add qstn and ex linear search

---
+++ Step3-Solve-problems-on-array/Lec1-Easy/linear_search.js
@@ -0,0 +1,6 @@
+// Input: nums = [2, 3, 4, 5, 3], target = 3
+// Output: 1
+// Explanation: The first occurence of 3 in nums is at index 1
+// Input: nums = [2, -4, 4, 0, 10], target = 6
+// Output: -1
+// Explanation: The value 6 does not occur in the array, hence output is -1
