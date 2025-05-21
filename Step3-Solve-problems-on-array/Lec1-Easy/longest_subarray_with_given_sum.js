60474a6cfa97823fc8f262bf1afb1f467fce23e7
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-21 10:42:47 +0530

    add example

---
+++ Step3-Solve-problems-on-array/Lec1-Easy/longest_subarray_with_given_sum.js
@@ -0,0 +1,4 @@
+// Input: nums = [10, 5, 2, 7, 1, 9],  k=15
+// Output: 4
+// Explanation: The longest sub-array with a sum equal to 15 is [5, 2, 7, 1], which has a length of 4. This sub-array starts at index 1 and ends at index 4, and the sum of its elements 
+// (5 + 2 + 7 + 1) equals 15. Therefore, the length of this sub-array is 4.
