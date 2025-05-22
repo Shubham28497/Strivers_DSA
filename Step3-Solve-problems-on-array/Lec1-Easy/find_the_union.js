6d909440b214e0fb39a082154e30071ec21771ea
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-22 09:19:01 +0530

    add logic

--- Step3-Solve-problems-on-array/Lec1-Easy/find_the_union.js
+++ Step3-Solve-problems-on-array/Lec1-Easy/find_the_union.js
@@ -5,3 +5,12 @@
 // Input: nums1 = [3, 4, 6, 7, 9, 9], nums2 = [1, 5, 7, 8, 8]
 // Output: [1, 3, 4, 5, 6, 7, 8, 9]
 // Explanation: The element 7 is common to both, 3, 4, 6, 9 are from nums1 and 1, 5, 8 is from nums2
+
+
+function unionUsingSet(arr1,arr2){
+    return [...new Set([...arr1,...arr2])]
+}
+
+arr1 = [1, 2, 3, 4];
+arr2 = [3, 4, 5, 6];
+console.log(unionUsingSet(arr1,arr2))
