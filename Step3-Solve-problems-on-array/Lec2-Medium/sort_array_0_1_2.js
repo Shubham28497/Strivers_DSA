ec1cbb9e482b3d609614b1846af05c371d0f9d8f
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-23 12:36:33 +0530

    add brute force

--- Step3-Solve-problems-on-array/Lec2-Medium/sort_array_0_1_2.js
+++ Step3-Solve-problems-on-array/Lec2-Medium/sort_array_0_1_2.js
@@ -5,3 +5,33 @@
 // Input: nums = [0, 0, 1, 1, 1]
 // Output: [0, 0, 1, 1, 1]
 // Explanation: The nums array in sorted order has 2 zeroes, 3 ones and zero twos
+// 1.Brute force approach
+function Sort012(arr){
+    let c0=0;
+    let c1=0;
+    let c2=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]===0) {
+            c0++
+        }
+        else if(arr[i]===1){
+            c1++
+        }
+        else if(arr[i]===2){
+            c2++
+        }
+    }
+    let index=0;
+    for(let i=0;i<c0;i++){
+        arr[index++]=0
+    }
+    for(let i=0;i<c1;i++){
+        arr[index++]=1
+    }
+    for(let i=0;i<c2;i++){
+        arr[index++]=2
+    }
+}
+let arr = [1, 0, 2, 1, 0]
+Sort012(arr)
+console.log(arr)
