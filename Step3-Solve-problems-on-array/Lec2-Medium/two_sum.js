cbd848606559a8d3cf19deab307165f2f9ce6bd8
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-23 09:41:35 +0530

    add 2nd approach

--- Step3-Solve-problems-on-array/Lec2-Medium/two_sum.js
+++ Step3-Solve-problems-on-array/Lec2-Medium/two_sum.js
@@ -20,3 +20,19 @@ let arr = [1, 3, 5, -7, 6, -3]
 let target = 0
 console.log(twoSum(arr,target))
 // TC:O(n^2)
+
+// 2.Better appraoch
+function twoSum(nums, target) {
+    const seen = new Map(); // number -> index
+    for (let i = 0; i < nums.length; i++) {
+      const complement = target - nums[i];
+      if (seen.has(complement)) {
+        return [seen.get(complement), i];
+      }
+      seen.set(nums[i], i);
+    }
+    return null;
+  }
+  let arr2 = [1, 6, 2, 10, 3]
+  let target2 = 7
+  console.log(twoSum(arr2,target2))
