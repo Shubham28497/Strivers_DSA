4c61be433bd729c0ee132abf3a6c1f9c4fb5215b
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-23 09:03:58 +0530

    add ex of two sum

---
+++ Step3-Solve-problems-on-array/Lec2-Medium/two_sum.js
@@ -0,0 +1,7 @@
+// Examples:
+// Input: nums = [1, 6, 2, 10, 3], target = 7
+// Output: [0, 1]
+// Explanation: nums[0] + nums[1] = 1 + 6 = 7
+// Input: nums = [1, 3, 5, -7, 6, -3], target = 0
+// Output: [1, 5]
+// Explanation: nums[1] + nums[5] = 3 + (-3) = 0
