7bc36679ecd33ea894696944353cc3050fd0755d
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-23 14:37:30 +0530

    add brute force

---
+++ Step3-Solve-problems-on-array/Lec2-Medium/major_element_1.js
@@ -0,0 +1,24 @@
+// Examples:
+// Input: nums = [7, 0, 0, 1, 7, 7, 2, 7, 7]
+// Output: 7
+// Explanation: The number 7 appears 5 times in the 9 sized array
+// Input: nums = [1, 1, 1, 2, 1, 2]
+// Output: 1
+// Explanation: The number 1 appears 4 times in the 6 sized array
+// 1.Brute force
+function majorityElement(arr){
+    for(let i=0;i<arr.length;i++){
+        let count=0;
+        for(let j=0;j<arr.length;j++){
+            if(arr[i]===arr[j]){
+            count++    
+            }
+        }
+         if(count>Math.floor(arr.length/2)){
+        return arr[i]
+    }
+    }
+ }
+ let arr = [7, 0, 0, 1, 7, 7, 2, 7, 7]
+ console.log(majorityElement(arr))
+ 
