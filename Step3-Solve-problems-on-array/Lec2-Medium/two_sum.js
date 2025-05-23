f9738c8e0af7eba22205d6dcd5b17c4ef7cd7242
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-23 09:13:30 +0530

    add logic

--- Step3-Solve-problems-on-array/Lec2-Medium/two_sum.js
+++ Step3-Solve-problems-on-array/Lec2-Medium/two_sum.js
@@ -5,3 +5,17 @@
 // Input: nums = [1, 3, 5, -7, 6, -3], target = 0
 // Output: [1, 5]
 // Explanation: nums[1] + nums[5] = 3 + (-3) = 0
+// 1.Brute force
+function twoSum(arr,target){
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+        if(arr[i]+arr[j]===target){
+            return [i,j]
+        }           
+        }
+    }
+    
+}
+let arr = [1, 3, 5, -7, 6, -3]
+let target = 0
+console.log(twoSum(arr,target))
