842b55eecd761a73a1bd23205965b4bc959a7b71
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-23 09:53:39 +0530

    add example sort an array 0s 1s and 2s

---
+++ Step3-Solve-problems-on-array/Lec2-Medium/sort_array_0_1_2.js
@@ -0,0 +1,7 @@
+// Examples:
+// Input: nums = [1, 0, 2, 1, 0]
+// Output: [0, 0, 1, 1, 2]
+// Explanation: The nums array in sorted order has 2 zeroes, 2 ones and 1 two
+// Input: nums = [0, 0, 1, 1, 1]
+// Output: [0, 0, 1, 1, 1]
+// Explanation: The nums array in sorted order has 2 zeroes, 3 ones and zero twos
