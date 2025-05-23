6416ac44d75dc16170b0a9dbf0ae2719de96a250
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-23 17:17:23 +0530

    add brute force approach

--- Step3-Solve-problems-on-array/Lec2-Medium/Kadens_algo_max_subarray_sum.js
+++ Step3-Solve-problems-on-array/Lec2-Medium/Kadens_algo_max_subarray_sum.js
@@ -5,3 +5,18 @@
 // Input: nums = [-2, -3, -7, -2, -10, -4]
 // Output: -2
 // Explanation: The element on index 0 or index 3 make up the largest sum when taken as a subarray
+function maxSubArraySum(arr){
+    let maxSum=0;
+    for(let i=0;i<arr.length;i++){
+        let sum=0;
+        for(let j=0;j<arr.length;j++){
+            sum=sum+arr[j]
+            if(sum>maxSum){
+                maxSum=sum;
+            }
+        }
+    }
+    return maxSum
+}
+let arr= [2, 3, 5, -2, 7, -4]
+console.log(maxSubArraySum(arr))
