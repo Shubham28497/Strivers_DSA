09cc2c369065eb446ef2b056405e33aeaa8663be
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-23 17:16:37 +0530

    add example

---
+++ Step3-Solve-problems-on-array/Lec2-Medium/Kadens_algo_max_subarray_sum.js
@@ -0,0 +1,7 @@
+// Examples:
+// Input: nums = [2, 3, 5, -2, 7, -4]
+// Output: 15
+// Explanation: The subarray from index 0 to index 4 has the largest sum = 15
+// Input: nums = [-2, -3, -7, -2, -10, -4]
+// Output: -2
+// Explanation: The element on index 0 or index 3 make up the largest sum when taken as a subarray
