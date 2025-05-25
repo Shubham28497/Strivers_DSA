b93730e86e64cdc15a2cb807e52bd468750a80d7
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-25 10:47:49 +0530

    add logic

--- Step3-Solve-problems-on-array/Lec2-Medium/leaders_in_array.js
+++ Step3-Solve-problems-on-array/Lec2-Medium/leaders_in_array.js
@@ -13,3 +13,21 @@
 // Explanation: -5 is the rightmost element, -4 is the largest element 
 // in the index range [4, 5], 1 is the largest element in the 
 // index range [3, 5] and 5 is the largest element in the range [2, 5]
+function leadersInArray(arr){
+    let leaders=[]
+    for(let i=0;i<arr.length;i++){
+        isLeader=true;
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[j]>=arr[i]){
+                isLeader=false;
+                break;
+            }
+        }
+        if(isLeader){
+            leaders.push(arr[i])
+        }
+    }
+    return leaders
+}
+let arr=[1, 2, 5, 3, 1, 2]
+console.log(leadersInArray(arr))
