0f24e970c9fc062970185871bd8a324263f11809
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-26 14:48:34 +0530

    add sol

--- Step3-Solve-problems-on-array/Lec2-Medium/rotate_matrix_90.js
+++ Step3-Solve-problems-on-array/Lec2-Medium/rotate_matrix_90.js
@@ -4,3 +4,22 @@
 // Output: {{7, 4, 1}, 
 //                 {8, 5, 2}, 
 //                 {9, 6, 3}} 
+function rotate(matrix){
+    let n=matrix.length;
+    for(let i=0;i<n;i++){
+        for(let j=i+1;j<n;j++){
+            [matrix[i][j],matrix[j][i]]=[matrix[j][i],matrix[i][j]]
+        }
+    }
+    for(let i=0;i<n;i++){
+        matrix[i].reverse();    
+    }
+}
+let matrix = [
+  [100, 4, 200],
+  [1, 3, 2],
+  [7, 8, 9]
+];
+
+rotate(matrix);
+console.log(matrix);
