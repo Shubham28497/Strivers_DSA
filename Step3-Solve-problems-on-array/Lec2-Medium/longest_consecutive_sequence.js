9a2a9f0e8edc54c77fe0a59ad82f65b94421cb1c
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-26 12:26:46 +0530

    step by step execution

--- Step3-Solve-problems-on-array/Lec2-Medium/longest_consecutive_sequence.js
+++ Step3-Solve-problems-on-array/Lec2-Medium/longest_consecutive_sequence.js
@@ -27,3 +27,49 @@ function longestConsecutiveSequence(arr,k){
 let arr=[100, 4, 200, 1, 3, 2]
 console.log(longestConsecutiveSequence(arr))
 //TC: O(n^2)
+
+//EX: i = 0, nums[0] = 100
+// Start with currNum = 100, count = 1
+
+// Is 101 in nums? ❌ No → End sequence.
+
+// maxLen = max(0, 1) = 1
+
+// 2️⃣ i = 1, nums[1] = 4
+// currNum = 4, count = 1
+
+// Is 5 in nums? ❌ No
+
+// maxLen = max(1, 1) = 1
+
+// 3️⃣ i = 2, nums[2] = 200
+// currNum = 200, count = 1
+
+// Is 201 in nums? ❌ No
+
+// maxLen = max(1, 1) = 1
+
+// 4️⃣ i = 3, nums[3] = 1
+// currNum = 1, count = 1
+
+// Is 2 in nums? ✅ Yes → currNum = 2, count = 2
+
+// Is 3 in nums? ✅ Yes → currNum = 3, count = 3
+
+// Is 4 in nums? ✅ Yes → currNum = 4, count = 4
+
+// Is 5 in nums? ❌ No
+
+// maxLen = max(1, 4) = 4
+
+// ✅ Longest sequence so far: [1, 2, 3, 4]
+
+// 5️⃣ i = 4, nums[4] = 3
+// currNum = 3, count = 1
+
+// Is 4 in nums? ✅ → currNum = 4, count = 2
+
+// Is 5 in nums? ❌
+
+// maxLen = max(4, 2) = 4
+
