136ff5fa2ad94692975732f879cfaace755e0259
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-26 12:25:30 +0530

    add brute force approach

--- Step3-Solve-problems-on-array/Lec2-Medium/longest_consecutive_sequence.js
+++ Step3-Solve-problems-on-array/Lec2-Medium/longest_consecutive_sequence.js
@@ -8,3 +8,21 @@
 // Output: 9
 // Explanation: The longest sequence of consecutive elements in
 //  the array is [0, 1, 2, 3, 4, 5, 6, 7, 8], which has a length of 9. 
+
+//1. Brute force appraoch
+function longestConsecutiveSequence(arr,k){
+    let maxLength=0;
+    for(let i=0;i<arr.length;i++){
+        let curNum=arr[i];
+        let count=1;
+        while(arr.includes(curNum+1)){
+            curNum++;
+            count++
+            
+        }
+        maxLength=Math.max(maxLength,count)
+    }
+    return maxLength
+}
+let arr=[100, 4, 200, 1, 3, 2]
+console.log(longestConsecutiveSequence(arr))
