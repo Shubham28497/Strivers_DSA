e60e881b9154a2c654446c39a393a6a1acae1856
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-28 17:16:53 +0530

    add example

---
+++ Step3-Solve-problems-on-array/Lec3-Hard/longest_subarray_with_zero_sum.js
@@ -0,0 +1,12 @@
+// Example 1:
+// Input Format: N = 6, array[] = {9, -3, 3, -1, 6, -5}
+// Result: 5
+// Explanation: The following subarrays sum to zero:
+// {-3, 3} , {-1, 6, -5}, {-3, 3, -1, 6, -5}
+// Since we require the length of the longest subarray, our answer is 5!
+
+// Example 2:
+// Input Format: N = 8, array[] = {6, -2, 2, -8, 1, 7, 4, -10}
+// Result: 8
+// Subarrays with sum 0 : {-2, 2}, {-8, 1, 7}, {-2, 2, -8, 1, 7}, {6, -2, 2, -8, 1, 7, 4, -10}
+// Length of longest subarray = 8
