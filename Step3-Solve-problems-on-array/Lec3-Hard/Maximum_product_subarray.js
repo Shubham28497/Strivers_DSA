556ea17b244b3ba4b28ea5bbffc54ac5e9b925b4
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-28 09:52:27 +0530

    add solution

--- Step3-Solve-problems-on-array/Lec3-Hard/Maximum_product_subarray.js
+++ Step3-Solve-problems-on-array/Lec3-Hard/Maximum_product_subarray.js
@@ -12,3 +12,16 @@
 //  20
 // Explanation:
 //  In the given array, we can see (-4)×(-5) gives maximum product value.
+function maxProductSubarray(arr){
+    let maxProduct=-Infinity
+    for(let i=0;i<arr.length;i++){
+        let prod=1;
+        for(let j=i;j<arr.length;j++){
+            prod=prod*arr[j];
+            maxProduct=Math.max(maxProduct,prod)
+        }
+    }
+    return maxProduct
+}
+let arr=[1,2,3,4,5,0]
+console.log(maxProductSubarray(arr))
