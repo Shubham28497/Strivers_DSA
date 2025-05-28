225f4ffeaf39a9178349192be2bcfbee6874994d
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-28 09:27:47 +0530

    add i/p and o/p

---
+++ Step3-Solve-problems-on-array/Lec2-Medium/spiral_matrix.js
@@ -0,0 +1,11 @@
+// Input: matrix = [[1, 2, 3], [4 ,5 ,6], [7, 8, 9]]
+
+// Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+// Explanation: The elements in the spiral order are 1, 2, 3 -> 6, 9 -> 8, 7 -> 4, 5
+
+// Input: matrix = [[1, 2, 3, 4], [5, 6, 7, 8]]
+
+// Output: [1, 2, 3, 4, 8, 7, 6, 5]
+
+// Explanation: The elements in the spiral order are 1, 2, 3, 4 -> 8, 7, 6, 5
