5be4518d6b28c8e2524bfb54cd0bcff1e5856286
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-29 11:14:57 +0530

    add example

---
+++ Step5-Strings/Easy/largest_odd_no_in_string.js
@@ -0,0 +1,10 @@
+// Example 1:
+
+// Input: num = "52"
+// Output: "5"
+// Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+// Example 2:
+
+// Input: num = "4206"
+// Output: ""
+// Explanation: There are no odd numbers in "4206".
