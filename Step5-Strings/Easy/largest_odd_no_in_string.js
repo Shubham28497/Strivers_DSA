db3c65ea3da19f3c1e25c36de39baf91887c4da8
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-29 12:20:43 +0530

    add logic

--- Step5-Strings/Easy/largest_odd_no_in_string.js
+++ Step5-Strings/Easy/largest_odd_no_in_string.js
@@ -8,3 +8,16 @@
 // Input: num = "4206"
 // Output: ""
 // Explanation: There are no odd numbers in "4206".
+function largestOddNumber (num){
+for(let i=num.length-1;i>=0;i--){
+    let digit=parseInt(num[i])
+    if(digit%2===1){
+        return num.slice(0,i+1)
+    }
+}
+return ""
+}
+let num = "4206"
+console.log(largestOddNumber(num))
+// console.log("Output:", `"${largestOddNumber(num)}"`);
+
