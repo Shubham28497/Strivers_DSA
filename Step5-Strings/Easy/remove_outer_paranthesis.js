cac58e93ff52d7a8267e4dc0fdbbf007941e220b
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-29 10:30:38 +0530

    add logic

--- Step5-Strings/Easy/remove_outer_paranthesis.js
+++ Step5-Strings/Easy/remove_outer_paranthesis.js
@@ -12,3 +12,24 @@
 // Explanation: 
 // The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
 // After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+function removeOuterParentheses (s){
+   let res='';
+   let open=0;
+   for(let char of s){
+       if(char==='('){
+           if(open>0){
+               res=res+char;
+           }
+           open++
+       }
+       else if(char===')'){
+           open--
+           if(open>0){
+               res=res+char
+           }
+       }
+   }
+   return res
+}
+let s = "(()())(())"
+console.log(removeOuterParentheses(s))
