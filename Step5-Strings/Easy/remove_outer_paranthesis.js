9fca81357fa2638394c1a41291ef5da339d77d20
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-29 10:30:16 +0530

    add example of remove outer paranthesis with io op

---
+++ Step5-Strings/Easy/remove_outer_paranthesis.js
@@ -0,0 +1,14 @@
+// Example 1:
+
+// Input: s = "(()())(())"
+// Output: "()()()"
+// Explanation: 
+// The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+// After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+// Example 2:
+
+// Input: s = "(()())(())(()(()))"
+// Output: "()()()()(())"
+// Explanation: 
+// The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+// After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
