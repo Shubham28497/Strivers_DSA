6efaa1bf6967df49e94f4905d65a81ae995713ed
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-29 09:29:35 +0530

    add reverse character logic

--- Step5-Strings/Easy/reverse_word_in_string.js
+++ Step5-Strings/Easy/reverse_word_in_string.js
@@ -15,3 +15,15 @@ function reverseString(s){
 }
 let s = "  Hello   world from   JS  ";
 console.log(reverseString(s));
+
+// 2.Reverse each Character
+function reverseString(str){
+     let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+   
+}
+let str = "Hello world";
+console.log(reverseString(str));
