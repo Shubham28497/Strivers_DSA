a95550b75c5613412ff847804dd45aea1f8a6a1b
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-05-29 09:14:57 +0530

    add logic to reverse word

--- Step5-Strings/Easy/reverse_word_in_string.js
+++ Step5-Strings/Easy/reverse_word_in_string.js
@@ -2,3 +2,16 @@
 // Output: "jungle the to welcome"
 // Input: s = " amazing coding skills "
 // Output: "skills coding amazing"
+function reverseString(s){
+    let words= s.split(' ')
+    let res=[]
+    for(let i=words.length-1;i>=0;i--){
+        if(words[i]){
+            res.push(words[i])
+        }
+    }
+    return res.join(' ')
+   
+}
+let s = "  Hello   world from   JS  ";
+console.log(reverseString(s));
