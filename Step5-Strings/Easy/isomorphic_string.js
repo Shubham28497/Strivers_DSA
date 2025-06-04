876a4ea54252eb21808163784610b7572ed8a2aa
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-04 17:16:22 +0530

    add sol and tc

--- Step5-Strings/Easy/isomorphic_string.js
+++ Step5-Strings/Easy/isomorphic_string.js
@@ -16,3 +16,23 @@
 
 // All the "b"s in t have to take places of "a", "p", "l", which requires "p" to be mapped to "b", but that
 //  makes it impossible for "p" at index 2 (0-indexed) to become "n". Thus no solution exists
+
+function isIsomorphic(s, t) {
+    if (s.length !== t.length) return false;
+
+    const mapS = new Array(256).fill(0);
+    const mapT = new Array(256).fill(0);
+
+    for (let i = 0; i < s.length; i++) {
+        const codeS = s.charCodeAt(i);
+        const codeT = t.charCodeAt(i);
+
+        if (mapS[codeS] !== mapT[codeT]) return false;
+
+        mapS[codeS] = i + 1;
+        mapT[codeT] = i + 1;
+    }
+
+    return true;
+}
+//TC:O(n)
