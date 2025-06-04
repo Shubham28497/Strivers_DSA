8389b2ca1cead8751ec53515c6b87fb5d68395c0
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-04 14:09:32 +0530

    add isomorphic string ex and qstn

---
+++ Step5-Strings/Easy/isomorphic_string.js
@@ -0,0 +1,18 @@
+// Input : s = "egg" , t = "add"
+
+// Output : true
+
+// Explanation : The 'e' in string s can be replaced with 'a' of string t.
+
+// The 'g' in string s can be replaced with 'd' of t.
+
+// Hence all characters in s can be replaced to get t.
+
+// Input : s = "apple" , t = "bbnbm"
+
+// Output : false
+
+// Explanation : It can be proved that no solution exists for this example.
+
+// All the "b"s in t have to take places of "a", "p", "l", which requires "p" to be mapped to "b", but that
+//  makes it impossible for "p" at index 2 (0-indexed) to become "n". Thus no solution exists
