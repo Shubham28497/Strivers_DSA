3c8b9bdd58375db74a2889ec2d5ef5b1333d35ee
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-04 12:27:11 +0530

    add example

---
+++ Step5-Strings/Easy/anagrams.js
@@ -0,0 +1,5 @@
+// Input : s = "anagram" , t = "nagaram"
+
+// Output : true
+
+// Explanation : We can rearrange the characters of string s to get string t as frequency of all characters from both strings is same.
