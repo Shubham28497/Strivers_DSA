20dc0de012a7501ae81ae36d7dce37a256e591cb
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-04 12:28:11 +0530

    add sol

--- Step5-Strings/Easy/anagrams.js
+++ Step5-Strings/Easy/anagrams.js
@@ -3,3 +3,9 @@
 // Output : true
 
 // Explanation : We can rearrange the characters of string s to get string t as frequency of all characters from both strings is same.
+function areAnagrams (s1,s2){
+return s1.split('').sort().join()===s2.split('').sort().join()
+}
+const s1 = "geeks";
+const s2 = "kseeg";
+console.log(areAnagrams(s1, s2));
