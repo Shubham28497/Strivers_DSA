e0deba823122a0efa535ca6d99284d7bc79afa60
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-04 12:53:21 +0530

    2nd approach to check string are anagrams or not

--- Step5-Strings/Easy/anagrams.js
+++ Step5-Strings/Easy/anagrams.js
@@ -10,3 +10,24 @@ const s1 = "geeks";
 const s2 = "kseeg";
 console.log(areAnagrams(s1, s2));
 // Time Complexity: O(m*log(m) + n*log(n))
+
+function areAnagrams (s1,s2){
+    if(s1.length!==s2.length){
+        return false
+    }
+let freq=new Array(26).fill(0)
+for(let ch of s1){
+    freq[ch.toLowerCase().charCodeAt(0)-'a'.charCodeAt(0)]++;
+}
+for(let ch of s2){
+    freq[ch.toLowerCase().charCodeAt(0)-'a'.charCodeAt(0)]--;
+}
+ for (let count of freq) {
+        if (count !== 0) return false;
+    }
+
+    return true;
+}
+const s3 = "geeks";
+const s4 = "kseeg";
+console.log(areAnagrams(s3, s4));
