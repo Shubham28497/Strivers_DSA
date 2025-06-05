0e85a23a717d3bbce70d33bc70a5e63f50dcf922
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-05 11:08:57 +0530

    add sol

--- Step5-Strings/Easy/check_one_string_rotation_another.js
+++ Step5-Strings/Easy/check_one_string_rotation_another.js
@@ -9,3 +9,15 @@
 // Input: s1 = "abcd", s2 = "acbd"
 // Output: false
 // Explanation: Strings are not rotations of each other.
+function isRotationConcat (str1,str2){
+    if(str1.length!==str2.length){
+        return false
+    }
+    return (str1+str1).includes(str2)
+   
+}
+console.log(isRotationConcat("waterbottle", "erbottlewat")); // true
+console.log(isRotationConcat("hello", "lohel"));             // true
+console.log(isRotationConcat("hello", "helol"));             // false
+
+
