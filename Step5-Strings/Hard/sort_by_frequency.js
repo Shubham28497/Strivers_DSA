31c9b5a9a79fa4056c808e4a7e23f27d93fde7f7
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-05 12:08:20 +0530

    add qstn and example

---
+++ Step5-Strings/Hard/sort_by_frequency.js
@@ -0,0 +1,21 @@
+// Input : s = "tree"
+
+// Output : ['e', 'r', 't' ]
+
+// Explanation : The occurrences of each character are as shown below :
+
+// e --> 2
+
+// r --> 1
+
+// t --> 1.
+
+// The r and t have same occurrences , so we arrange them by alphabetic order.
+
+// Input : s = "raaaajj"
+
+// Output : ['a' , 'j', 'r' ]
+
+// Explanation : The occurrences of each character are as shown below :
+
+// a --> 4
