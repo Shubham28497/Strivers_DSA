7c642b15d8f542847909c778b94a71c5c4839c05
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-06 14:19:24 +0530

    add sol

--- Step5-Strings/Hard/maximum_nesting_depth_paranthesis.js
+++ Step5-Strings/Hard/maximum_nesting_depth_paranthesis.js
@@ -8,3 +8,27 @@
 // Output: 3
 // Explanation:
 // Digit 3 is inside of 3 nested parentheses in the string.
+
+function maxParenthesisDepth (s){
+ let depth=0;
+ let maxDepth=0;
+ for(let ch of s){
+     if(ch==='('){
+         depth++;
+         maxDepth=Math.max(maxDepth,depth)
+     }
+     else if(ch===')'){
+         depth--;
+         if(depth<0){
+             return -1;
+         }
+     }
+ }
+ return depth===0?maxDepth:-1
+}
+console.log(maxParenthesisDepth("(1+(2*3)+((8)/4))+1")); // Output: 3
+console.log(maxParenthesisDepth("(1)+((2))+(((3)))"));   // Output: 3
+console.log(maxParenthesisDepth("1+(2*3)/(2-1)"));       // Output: 1
+console.log(maxParenthesisDepth("((())"))               // Output: -1 (unbalanced)
+
+
