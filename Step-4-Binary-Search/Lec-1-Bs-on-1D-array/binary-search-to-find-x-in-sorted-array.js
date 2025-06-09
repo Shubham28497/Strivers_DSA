0732861c355916cd4c7e9974dacd682a505dd0bd
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-09 14:54:15 +0530

    add binary search

--- Step-4-Binary-Search/Lec-1-Bs-on-1D-array/binary-search-to-find-x-in-sorted-array.js
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/binary-search-to-find-x-in-sorted-array.js
@@ -23,4 +23,26 @@ let arr=[-1,0,3,5,9,12]
 let target= 9
 console.log(binarySearch(arr,target))
 
+// 2.Binary search
+function binarySearch(arr,target){
+  let low=0;
+  let high=arr.length-1;
+  while(low<=high){
+      let mid=Math.floor((low+high)/2)
+      if(arr[mid]==target){
+          return mid;
+      }
+      else if(arr[mid]<target){
+          low=mid+1;
+      }
+      else{
+          high=mid-1
+      }
+  }
+  return -1;
+}
+let arr2=[-1,0,3,5,9,12]
+let target2= 9
+console.log(binarySearch(arr2,target2))
+
 
