2a9f26ce725766bf173e4326b120e3618c9107b5
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-10 09:11:05 +0530

    add qstn

---
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/implement_lower_bound.js
@@ -0,0 +1,9 @@
+// Example 1:
+// Input Format: N = 4, arr[] = {1,2,2,3}, x = 2
+// Result: 1
+// Explanation: Index 1 is the smallest index such that arr[1] >= x.
+
+// Example 2:
+// Input Format: N = 5, arr[] = {3,5,8,15,19}, x = 9
+// Result: 3
+// Explanation: Index 3 is the smallest index such that arr[3] >= x
