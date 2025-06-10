f6d767926365af56d0a79b07e4c21e8bfc2cbf09
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-10 16:19:40 +0530

    add qstn

---
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/search_in_rotated_sorted_array_1.js
@@ -0,0 +1,12 @@
+// Examples:
+// Input : nums = [4, 5, 6, 7, 0, 1, 2], k = 0
+
+// Output: 4
+
+// Explanation: Here, the target is 0. We can see that 0 is present in the given rotated sorted array, nums. Thus, we get output as 4, which is the index at which 0 is present in the array.
+
+// Input: nums = [4, 5, 6, 7, 0, 1, 2], k = 3
+
+// Output: -1
+
+// Explanation: Here, the target is 3. Since 3 is not present in the given rotated sorted array. Thus, we get the output as -1.
