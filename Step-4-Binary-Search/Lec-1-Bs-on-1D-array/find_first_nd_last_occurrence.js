28827266afb01e550a2d47604bd3133124c97235
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-10 10:22:23 +0530

    add qstn

---
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/find_first_nd_last_occurrence.js
@@ -0,0 +1,11 @@
+// Input: nums = [5, 7, 7, 8, 8, 10], target = 8
+
+// Output: [3, 4]
+
+// Explanation:The target is 8, and it appears in the array at indices 3 and 4, so the output is [3,4]
+
+// Input: nums = [5, 7, 7, 8, 8, 10], target = 6
+
+// Output: [-1, -1]
+
+// Expalantion: The target is 6, which is not present in the array. Therefore, the output is [-1, -1].
