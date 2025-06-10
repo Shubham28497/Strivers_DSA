dd6d52f1b3e7cf20f201840d62916676e28a2dbc
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-10 09:26:33 +0530

    add solution

--- Step-4-Binary-Search/Lec-1-Bs-on-1D-array/implement_lower_bound.js
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/implement_lower_bound.js
@@ -7,3 +7,24 @@
 // Input Format: N = 5, arr[] = {3,5,8,15,19}, x = 9
 // Result: 3
 // Explanation: Index 3 is the smallest index such that arr[3] >= x
+function lowerBound(arr,target){
+  let low=0;
+  let high=arr.length-1;
+  let res=arr.length;
+  while(low<=high){
+      let mid=Math.floor((low+high)/2)
+      if(arr[mid]>=target){
+          res=mid;
+          high=mid-1;
+      }
+      else{
+          low=mid+1
+      }
+  }
+  return res;
+}
+let arr=[-1,0,3,5,9,12]
+let target= 9
+console.log(lowerBound(arr,target))
+
+
