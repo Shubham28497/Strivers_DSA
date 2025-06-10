9af1cd84fd6b39208df1ceae5eaf956e2c452d35
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-10 10:45:46 +0530

    add log with linear search

--- Step-4-Binary-Search/Lec-1-Bs-on-1D-array/find_first_nd_last_occurrence.js
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/find_first_nd_last_occurrence.js
@@ -9,3 +9,22 @@
 // Output: [-1, -1]
 
 // Expalantion: The target is 6, which is not present in the array. Therefore, the output is [-1, -1].
+// 1.with linear search
+function firstAndLastOccurrence(arr,target){
+  let first=-1;
+  let last=-1;
+  for(let i=0;i<arr.length;i++){
+      if(arr[i]==target){
+          if(first===-1){
+              first=i
+          }
+          last=i;
+  }
+}
+return [first,last]
+}
+let arr=[5, 7, 7, 8, 8, 10]
+let target= 8
+console.log(firstAndLastOccurrence(arr,target))
+
+
