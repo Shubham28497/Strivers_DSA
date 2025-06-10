5039d046e070fae1781bc5abc91344563d83be70
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-10 11:24:17 +0530

    add logic with binary search

--- Step-4-Binary-Search/Lec-1-Bs-on-1D-array/find_first_nd_last_occurrence.js
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/find_first_nd_last_occurrence.js
@@ -27,5 +27,54 @@ let arr=[5, 7, 7, 8, 8, 10]
 let target= 8
 console.log(firstAndLastOccurrence(arr,target))
 //Tc:O(N)
+//2. with binary approach
+function firstOccurrence(arr,target){
+  let low=0;
+  let high=arr.length-1;
+  let first=-1;
+  while(low<=high){
+      let mid=Math.floor((low+high)/2)
+      if(arr[mid]==target){
+          first=mid;
+          high=mid-1
+      }
+      else if(arr[mid]<target){
+          low=mid+1;
+      }
+      else{
+          high=mid-1
+      }
+  }
+  return first
+}
+function lastOccurrence(arr,target){
+  let low=0;
+  let high=arr.length-1;
+  let last=-1;
+  while(low<=high){
+      let mid=Math.floor((low+high)/2)
+      if(arr[mid]==target){
+          last=mid;
+          low=mid+1
+      }
+      else if(arr[mid]<target){
+          low=mid+1;
+      }
+      else{
+          high=mid-1
+      }
+  }
+  return last
+}
+function find(arr,target){
+     let first = firstOccurrence(arr, target);
+    let last = lastOccurrence(arr, target);
+    return [first, last];
+}
+let arr2=[1, 3, 5, 5, 5, 5, 67, 123, 125]
+let target2= 5
+console.log(find(arr2,target2))
+
+
 
 
