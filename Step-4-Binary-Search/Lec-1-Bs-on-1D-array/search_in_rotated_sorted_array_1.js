89af9b5a612c00fabe3ff7fdee0ddac1d1b08572
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-10 21:35:28 +0530

    add sol

--- Step-4-Binary-Search/Lec-1-Bs-on-1D-array/search_in_rotated_sorted_array_1.js
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/search_in_rotated_sorted_array_1.js
@@ -10,3 +10,35 @@
 // Output: -1
 
 // Explanation: Here, the target is 3. Since 3 is not present in the given rotated sorted array. Thus, we get the output as -1.
+function searchElementUnique(arr,target){
+  let low=0;
+  let high=arr.length-1;
+  while(low<=high){
+      let mid=Math.floor((low+high)/2)
+      if(arr[mid]==target){
+          return mid;
+      }
+      if(arr[low]<=arr[mid]){
+          if(arr[low]<=target && target<=arr[mid]){
+              high=mid-1;
+          }
+          else{
+              low=mid+1;
+          }
+      }
+      else{
+          if(arr[mid]<=target && target<=arr[high]){
+              low=mid+1;
+          }
+          else{
+              high=mid-1;
+          }
+      }
+  }
+  return -1;
+}
+let arr=[4, 5, 6, 7, 0, 1, 2]
+let target= 0
+console.log(searchElementUnique(arr,target))
+
+
