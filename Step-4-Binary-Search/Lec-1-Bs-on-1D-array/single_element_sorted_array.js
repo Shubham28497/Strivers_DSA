dff6906495e29b089bcdac201c6bcad5960d8f1d
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-11 10:05:56 +0530

    add qstn

---
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/single_element_sorted_array.js
@@ -0,0 +1,11 @@
+// Input :nums = [1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6]
+
+// Output:4
+
+// Explanation: Only the number 4 appears once in the array.
+
+// Input : nums = [1, 1, 3, 5, 5]
+
+// Output:3
+
+// Explanation: Only the number 3 appears once in the array.
