929529fbcc5202aeb0eb3a4c0531f71aca4505f0
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-11 09:47:20 +0530

    add qstn

---
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/find_how_many_times_array_rotated.js
@@ -0,0 +1,11 @@
+// Input : nums = [4, 5, 6, 7, 0, 1, 2, 3]
+
+// Output: 4
+
+// Explanation: The original array should be [0, 1, 2, 3, 4, 5, 6, 7]. So, we can notice that the array has been rotated 4 times.
+
+// Input: nums = [3, 4, 5, 1, 2]
+
+// Output: 3
+
+// Explanation: The original array should be [1, 2, 3, 4, 5]. So, we can notice that the array has been rotated 3 times.
