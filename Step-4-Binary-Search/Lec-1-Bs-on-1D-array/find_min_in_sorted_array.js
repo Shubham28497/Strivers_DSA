c47fb22dbd1cb4104dc53af940264421a1c05f76
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-11 09:42:35 +0530

    add logic

--- Step-4-Binary-Search/Lec-1-Bs-on-1D-array/find_min_in_sorted_array.js
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/find_min_in_sorted_array.js
@@ -9,3 +9,24 @@
 // Output: 1
 
 // Explanation:Here, the element 1 is the minimum element in the array.
+function minSorted(arr){
+    let low=0;
+    let high=arr.length-1;
+    let ans=Infinity
+    while(low<=high){
+        let mid=Math.floor((low+high)/2)
+        if(arr[low]<=arr[mid]){
+            ans=Math.min(ans,arr[low])
+            low=mid+1;
+        }
+        else{
+            ans=Math.min(ans,arr[mid])
+            high=mid-1;
+        }
+    }
+    return ans
+}
+let arr=[4,5,6,7,0,1,2]
+console.log(minSorted(arr))
+
+
