6eca825a6fe72a41f0dcadc7c06a10b8fea6636e
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-11 09:07:24 +0530

    add qstn

---
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/find_min_in_sorted_array.js
@@ -0,0 +1,11 @@
+// Input : nums = [4, 5, 6, 7, 0, 1, 2, 3]
+
+// Output: 0
+
+// Explanation: Here, the element 0 is the minimum element in the array.
+
+// Input : nums = [3, 4, 5, 1, 2]
+
+// Output: 1
+
+// Explanation:Here, the element 1 is the minimum element in the array.
