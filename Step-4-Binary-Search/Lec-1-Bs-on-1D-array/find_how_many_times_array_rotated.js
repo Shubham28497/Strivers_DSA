942fcc42f5284f6b276325eb8560c0a275590259
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-11 10:01:42 +0530

    add sol

--- Step-4-Binary-Search/Lec-1-Bs-on-1D-array/find_how_many_times_array_rotated.js
+++ Step-4-Binary-Search/Lec-1-Bs-on-1D-array/find_how_many_times_array_rotated.js
@@ -9,3 +9,30 @@
 // Output: 3
 
 // Explanation: The original array should be [1, 2, 3, 4, 5]. So, we can notice that the array has been rotated 3 times.
+function countRotations(arr){
+    let low=0;
+    let high=arr.length-1;
+    let ans=Infinity
+    let index=-1;
+    while(low<=high){
+        let mid=Math.floor((low+high)/2)
+        if(arr[low]<=arr[mid]){
+           if(arr[low]<ans){
+            ans=arr[low]
+            index=low;
+           }
+           low=mid+1;
+        }
+        else{
+            if(arr[mid]<ans){
+                ans=arr[mid]
+                index=mid;
+            }
+            high=mid-1;
+        }
+    }
+    return index
+}
+let arr=[4,5,6,7,0,1,2]
+console.log(countRotations(arr))
+//TC:O(logn)
