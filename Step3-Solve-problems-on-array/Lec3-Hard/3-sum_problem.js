eccb3a52dae0cd4e53277ef927ab01570141ef62
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-17 10:55:30 +0530

    updated

--- Step3-Solve-problems-on-array/Lec3-Hard/3-sum_problem.js
+++ Step3-Solve-problems-on-array/Lec3-Hard/3-sum_problem.js
@@ -1,21 +1,12 @@
 // Input: nums = [2, -2, 0, 3, -3, 5]
-
 // Output: [[-2, 0, 2], [-3, -2, 5], [-3, 0, 3]]
-
 // Explanation: nums[1] + nums[2] + nums[0] = 0
-
 // nums[4] + nums[1] + nums[5] = 0
-
 // nums[4] + nums[2] + nums[3] = 0
-
 // Input: nums = [2, -1, -1, 3, -1]
-
 // Output: [[-1, -1, 2]]
-
 // Explanation: nums[1] + nums[2] + nums[0] = 0
-
 // Note that we have used two -1s as they are separate elements with different indexes
-
 // But we have not used the -1 at index 4 as that would create a duplicate triplet
 function threeSum(arr) {
   const result = new Set();
