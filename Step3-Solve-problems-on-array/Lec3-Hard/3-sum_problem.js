053efe481ffced3e25bdb7e863261eb3b92e5163
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-17 12:02:25 +0530

    add two pointer approach for 3sum

--- Step3-Solve-problems-on-array/Lec3-Hard/3-sum_problem.js
+++ Step3-Solve-problems-on-array/Lec3-Hard/3-sum_problem.js
@@ -34,8 +34,39 @@ function threeSum(arr) {
 // 🔁 3. .map(Number)
 // This part converts each string "0", "-1", "1" to actual numbers:
 // ["-1", "0", "1"] → [-1, 0, 1]
-
 let arr = [-1, 0, 1, 2, -1, -4];
 console.log(threeSum(arr));
 // Output: [ [ -1, -1, 2 ], [ -1, 0, 1 ] ]
 //TC:O(n^3)
+
+//2. two pinter approach
+function threeSum(arr) {
+const result=[];
+arr.sort((a,b)=>a-b);
+for(let i=0;i<arr.length-2;i++){
+    if(i>0 && arr[i]==arr[i-1]) continue;
+    let left=i+1;
+    let right=arr.length-1;
+    while(left<right){
+        const sum=arr[i]+arr[left]+arr[right]
+        if(sum===0){
+            result.push(arr[i],arr[left],arr[right])
+        
+        while(left<right && arr[left]=== arr[left+1]) left++
+        while(left<right && arr[right]=== arr[right+1]) right--
+        left++
+        right--
+    } else if(sum<0){
+        left++
+    }
+    else{
+        right--
+    }
+    }
+}
+return result
+}
+
+let arr2 = [-1, 0, 1, 2, -1, -4];
+console.log(threeSum(arr2));
+// Output: [ [ -1, -1, 2 ], [ -1, 0, 1 ] ]
