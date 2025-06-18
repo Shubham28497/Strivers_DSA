533a9e5b217a305d0a77ea73282459e647c84d05
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-18 15:16:55 +0530

    add count inversion example

---
+++ Step3-Solve-problems-on-array/Lec3-Hard/count_inversion.js
@@ -0,0 +1,11 @@
+// Input: nums = [2, 3, 7, 1, 3, 5]
+// Output: 5
+// Explanation: The responsible indexes are:
+// nums[0], nums[3], values: 2 > 1 & indexes: 0 < 3
+// nums[1], nums[3], values: 3 > 1 & indexes: 1 < 3
+// nums[2], nums[3], values: 7 > 1 & indexes: 2 < 3
+// nums[2], nums[4], values: 7 > 3 & indexes: 2 < 4
+// nums[2], nums[5], values: 7 > 5 & indexes: 2 < 5
+// Input: nums = [-10, -5, 6, 11, 15, 17]
+// Output: 0
+// Explanation: nums is sorted, hence no inversions present.
