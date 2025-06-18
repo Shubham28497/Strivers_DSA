38807b2e60dfe9295c1bbecb9b85718c2b42a911
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-18 10:40:30 +0530

    add sol

--- Step3-Solve-problems-on-array/Lec3-Hard/majority_element_2.js
+++ Step3-Solve-problems-on-array/Lec3-Hard/majority_element_2.js
@@ -10,3 +10,41 @@
 
 // Input: nums = [1,2]
 // Output: [1,2]
+
+function majorityElement(arr) {
+let count1=0; let count2=0;
+let el1; let el2;
+for(let i=0;i<arr.length-1;i++){
+    if(count1===0 && el1!=arr[i]){
+        count=1;
+        el1=arr[i]
+    }
+    else if(count2===0 && el2!=arr[i]){
+        count2=1;
+        el2=arr[i]
+    }
+    else if(arr[i]===el1){
+        count1++
+    }
+    else if(arr[i]===el2){
+        count2++
+    }
+    else{
+        count1--
+        count2--
+    }
+}
+count1=0;
+count2=0;
+let result=[]
+for(let i=0;i<arr.length;i++){
+    if(el1==arr[i]) count1++;
+    if(el2==arr[i]) count2++;
+}
+    if (count1 > Math.floor(arr.length / 3)) result.push(el1);
+    if (count2 > Math.floor(arr.length / 3)) result.push(el2);
+
+    return result;
+}
+let arr = [1, 2, 1, 1, 3, 2, 2];
+console.log(majorityElement(arr));
