289fd902fcce1092b6a6b6cb047359be7664771f
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-19 12:07:21 +0530

    add qstn

---
+++ Step3-Solve-problems-on-array/Lec3-Hard/missing_and_repeating.js
@@ -0,0 +1,6 @@
+// Input: nums = [3, 5, 4, 1, 1]
+// Output: [1, 2]
+// Explanation: 1 appears two times in the array and 2 is missing from nums
+// Input: nums = [1, 2, 3, 6, 7, 5, 7]
+// Output: [7, 4]
+// Explanation: 7 appears two times in the array and 4 is missing from nums.
