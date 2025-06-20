4fb0db60fbf8bab4e36be94d7476f088a22b65ea
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-20 10:26:26 +0530

    add qstn

---
+++ Step-4-Binary-Search/Lec-2-Bs-answers/median_two_sorted_array.js
@@ -0,0 +1,7 @@
+// Input: a[] = [-5, 3, 6, 12, 15], b[] = [-12, -10, -6, -3, 4, 10]
+// Output: 3
+// Explanation: The merged array is [-12, -10, -6, -5 , -3, 3, 4, 6, 10, 12, 15]. So the median of the merged array is 3.
+
+// Input: a[] = [1, 12, 15, 26, 38], b[] = [2, 13, 17, 30, 45, 60]
+// Output: The median is 17.
+// Explanation : The merged array is [1, 2, 12, 13, 15, 17, 26, 30, 38, 45, 60]. So the median of the merged array is 17.
