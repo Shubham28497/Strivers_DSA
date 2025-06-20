ef1971eb3169619bd8a1926b175d2d14b6c96ec2
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-20 10:27:01 +0530

    add brute force for median of 2 sorted array

--- Step-4-Binary-Search/Lec-2-Bs-answers/median_two_sorted_array.js
+++ Step-4-Binary-Search/Lec-2-Bs-answers/median_two_sorted_array.js
@@ -5,3 +5,27 @@
 // Input: a[] = [1, 12, 15, 26, 38], b[] = [2, 13, 17, 30, 45, 60]
 // Output: The median is 17.
 // Explanation : The merged array is [1, 2, 12, 13, 15, 17, 26, 30, 38, 45, 60]. So the median of the merged array is 17.
+function findMedianSortedArrays(arr1,arr2) {
+let merge=[]
+let i=0;
+let j=0;
+while(i<arr1.length && j<arr2.length){
+    if(arr1[i]<arr2[j]){
+        merge.push(arr1[i++])
+    }
+    else{
+        merge.push(arr2[j++])
+    }
+}
+ while(i<arr1.length) merge.push(arr1[i++])
+ while(j<arr2.length) merge.push(arr2[j++])
+ let n=merge.length
+ if(n%2===0){
+     return (merge[n/2]+merge[n/2-1])/2
+ }else{
+     return merge[Math.floor(n/2)]
+ }
+}
+let arr1 = [-5, 3, 6, 12, 15];
+let arr2 = [-12, -10, -6, -3, 4, 10];
+console.log(findMedianSortedArrays(arr1,arr2));
