5e8f4b7febc2d42b7b07d617cbbfd9c7e5994d85
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-20 09:26:00 +0530

    add logic brute force

--- Step-4-Binary-Search/Lec-2-Bs-answers/kth_missing_positive_number.js
+++ Step-4-Binary-Search/Lec-2-Bs-answers/kth_missing_positive_number.js
@@ -8,3 +8,15 @@
 // Input: arr = [1,2,3,4], k = 2
 // Output: 6
 // Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6
+function findKthMissingPositive(arr,k){
+let n=arr.length;
+for(let i=0;i<arr.length;i++){
+    if(arr[i]>(k+i)){
+        return k+i;
+    }
+}
+return k+n;
+}
+let arr = [2, 3, 4, 7, 11];
+let k = 5;
+console.log(findKthMissingPositive(arr, k));
