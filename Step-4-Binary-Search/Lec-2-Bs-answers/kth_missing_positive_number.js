0d90a75842dfec0ad9b63f0bb01cb4ac62baddb1
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-20 09:50:59 +0530

    add binary appraoch logic

--- Step-4-Binary-Search/Lec-2-Bs-answers/kth_missing_positive_number.js
+++ Step-4-Binary-Search/Lec-2-Bs-answers/kth_missing_positive_number.js
@@ -21,3 +21,25 @@ let arr = [2, 3, 4, 7, 11];
 let k = 5;
 console.log(findKthMissingPositive(arr, k));
 //TC:O(n)
+function kthMissing(arr, k) {
+    let lo = 0, hi = arr.length - 1;
+    let res = arr.length + k;
+
+    // Binary Search for index where arr[i] > (i + k)
+    while (lo <= hi) {
+        let mid = Math.floor((lo + hi) / 2);
+        if (arr[mid] > mid + k) {
+            res = mid + k;
+            hi = mid - 1;
+        } else {
+            lo = mid + 1;
+        }
+    }
+
+    return res;
+}
+
+// Driver Code
+const arr2 = [2, 3, 4, 7, 11];
+const k2 = 5;
+console.log(kthMissing(arr2, k2));
