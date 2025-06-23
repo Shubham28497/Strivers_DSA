db72bd3ca113b1937e9d229c381defe9b1f48dbb
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-23 14:31:24 +0530

    add steps

--- Step10-Sliding-window-2-pointer-problems/Lec-1-Medium-problems/Longest-Substring-Without-Repeating-Characters.js
+++ Step10-Sliding-window-2-pointer-problems/Lec-1-Medium-problems/Longest-Substring-Without-Repeating-Characters.js
@@ -44,3 +44,53 @@ console.log(lengthOfLongestSubstring(s))
 // Window: s[0...2] = 'abc'
 
 // Update maxLength = max(2, 2 - 0 + 1) = 3
+// Step 4: right = 3, s[right] = 'a'
+// 'a' is already in seen → we must shrink window from left
+
+// Remove s[left] = 'a' → seen = {'b', 'c'} → left = 1
+
+// Now 'a' not in seen, so add 'a' → seen = {'a', 'b', 'c'}
+
+// Window: s[1...3] = 'bca'
+
+// maxLength = max(3, 3 - 1 + 1) = 3
+
+// Step 5: right = 4, s[right] = 'b'
+// 'b' is in seen → remove s[left] = 'b' → seen = {'a', 'c'} → left = 2
+
+// Now add 'b' → seen = {'a', 'b', 'c'}
+
+// Window: s[2...4] = 'cab'
+
+// maxLength = max(3, 4 - 2 + 1) = 3
+
+// Step 6: right = 5, s[right] = 'c'
+// 'c' is in seen → remove s[left] = 'c' → seen = {'a', 'b'} → left = 3
+
+// Add 'c' → seen = {'a', 'b', 'c'}
+
+// Window: s[3...5] = 'abc'
+
+// maxLength = max(3, 5 - 3 + 1) = 3
+
+// Step 7: right = 6, s[right] = 'b'
+// 'b' is in seen → remove s[left] = 'a' → seen = {'b', 'c'} → left = 4
+
+// Remove s[left] = 'b' → seen = {'c'} → left = 5
+
+// Add 'b' → seen = {'c', 'b'}
+
+// Window: s[5...6] = 'cb'
+
+// maxLength = max(3, 6 - 5 + 1) = 3
+
+// Step 8: right = 7, s[right] = 'b'
+// 'b' is in seen → remove s[left] = 'c' → seen = {'b'} → left = 6
+
+// Remove s[left] = 'b' → seen = {} → left = 7
+
+// Add 'b' → seen = {'b'}
+
+// Window: s[7...7] = 'b'
+
+// maxLength = max(3, 7 - 7 + 1) = 3
