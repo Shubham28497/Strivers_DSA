6b6f953899a5671f3b2f230f05b5527f3c01ff20
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-23 14:24:54 +0530

    add solution

--- Step10-Sliding-window-2-pointer-problems/Lec-1-Medium-problems/Longest-Substring-Without-Repeating-Characters.js
+++ Step10-Sliding-window-2-pointer-problems/Lec-1-Medium-problems/Longest-Substring-Without-Repeating-Characters.js
@@ -8,3 +8,19 @@
 // Input: s = "bbbbb"
 // Output: 1
 // Explanation: The answer is "b", with the length of 1
+function lengthOfLongestSubstring(s){
+let left=0;
+let maxLength=0;
+let set=new Set()
+for(let right=0;right<s.length;right++){
+    while(set.has(s[right])){
+        set.delete(s[left])
+        left++
+    }
+    set.add(s[right])
+    maxLength=Math.max(maxLength,right-left+1)
+}
+return maxLength
+}
+let s="abcabcbb"
+console.log(lengthOfLongestSubstring(s))
