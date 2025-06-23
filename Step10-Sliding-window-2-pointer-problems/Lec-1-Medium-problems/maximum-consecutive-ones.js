ad22eca496f15a8778ee2e1414bad337b609bc5c
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-23 16:49:09 +0530

    add solution

--- Step10-Sliding-window-2-pointer-problems/Lec-1-Medium-problems/maximum-consecutive-ones.js
+++ Step10-Sliding-window-2-pointer-problems/Lec-1-Medium-problems/maximum-consecutive-ones.js
@@ -10,3 +10,23 @@
 // Output: 10
 // Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
 // Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+function longestOnes(arr,k){
+let left=0;
+let maxOnes=0;
+let zeroCount=0;
+for(let right=0;right<arr.length;right++){
+    if(arr[right]===0){
+        zeroCount++
+    }
+    while(zeroCount>k){
+        if(arr[left]===0){
+            zeroCount--
+        }
+        left++
+    }
+    maxOnes=Math.max(maxOnes,right-left+1)
+}
+return maxOnes
+}
+let arr=[1,1,1,0,0,0,1,1,1,1,0] , k=2
+console.log(longestOnes(arr,k))
