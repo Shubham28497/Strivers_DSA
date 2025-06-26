0e9162897fd726f2f936cda03c93b8fb7c11f1cd
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-06-26 10:19:40 +0530

    add sol

--- Step10-Sliding-window-2-pointer-problems/Lec-1-Medium-problems/count_number_of_nice_subarrays.js
+++ Step10-Sliding-window-2-pointer-problems/Lec-1-Medium-problems/count_number_of_nice_subarrays.js
@@ -8,3 +8,26 @@
 // Input: nums = [2,4,6], k = 1
 // Output: 0
 // Explanation: There are no odd numbers in the array.
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfSubarrays = function(nums, k) {
+    const atMost=(k)=>{
+    if(k<0) return 0;
+    let left=0 
+     let sum=0; 
+     let count=0;
+    for(let right=0; right<nums.length; right++){
+        if(nums[right]%2==1) sum+=1
+        while(sum>k){
+           if(nums[left]%2==1) sum-=1
+           left++
+        }
+        count+=right-left+1;
+    }
+    return count
+   }
+    return atMost(k) - atMost(k-1)
+};
