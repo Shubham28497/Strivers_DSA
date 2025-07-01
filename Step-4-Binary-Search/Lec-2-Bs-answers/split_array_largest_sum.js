8f4dd483b498dceff9101aff1958b5599ad8154a
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-07-01 11:45:13 +0530

    add solution

--- Step-4-Binary-Search/Lec-2-Bs-answers/split_array_largest_sum.js
+++ Step-4-Binary-Search/Lec-2-Bs-answers/split_array_largest_sum.js
@@ -14,3 +14,31 @@
 // Output: 9
 // Explanation: There are four ways to split nums into two subarrays.
 // The best way is to split it into [1,2,3] and [4,5], where the largest sum among the two subarrays is only 9.
+function splitArray(nums,k){
+    let n=nums.length;
+    let left=Math.max(...nums)
+    let right=nums.reduce((acc,num)=>acc+num,0)
+        // Binary search for the minimum possible largest sum
+    while(left<right){
+        let mid=Math.floor((left+right)/2)
+        let subArrayCount=1;
+        let currSum=0;
+        for(let i=0;i<n;i++){
+            if(currSum+nums[i]>mid){
+                subArrayCount+=1;
+                currSum=nums[i]
+            }
+            else{
+                currSum+=nums[i]
+            }
+        }
+        if(subArrayCount>k){
+            left=mid+1;
+        }
+        else{
+            right=mid;
+        }
+    }
+    return left;
+}
+console.log(splitArray([7, 2, 5, 10, 8], 2));
