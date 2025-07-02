12a493fc10ccacde9b4cb074062a421b825e5549
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-07-02 11:55:41 +0530

    add optimal approach

--- Step-4-Binary-Search/Lec-2-Bs-answers/median_two_sorted_array.js
+++ Step-4-Binary-Search/Lec-2-Bs-answers/median_two_sorted_array.js
@@ -30,3 +30,40 @@ let arr1 = [-5, 3, 6, 12, 15];
 let arr2 = [-12, -10, -6, -3, 4, 10];
 console.log(findMedianSortedArrays(arr1,arr2));
 //TC:O(n+m)
+
+//2. optimal approach
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var reversePairs = function(nums) {
+  function mergeSort(start,end){
+    if(start>=end) return 0;
+    let mid=Math.floor((start+end)/2);
+    let count=mergeSort(start,mid)+mergeSort(mid+1,end)
+    let j=mid+1;
+    for(let i=start;i<=mid;i++){
+        while(j<=end && nums[i]>2*nums[j]){
+            j++;
+        }
+        count+=j-(mid+1)
+    }
+    let temp=[];
+   let left=start;
+   let right=mid+1;
+   while(left<=mid && right<=end){
+    if(nums[left]<=nums[right]){
+        temp.push(nums[left++])
+    }else{
+        temp.push(nums[right++])
+    }
+   }
+   while(left<=mid) temp.push(nums[left++])
+   while(right<=end) temp.push(nums[right++])
+   for(let i=start;i<=end;i++){
+    nums[i]=temp[i-start]
+   }
+   return count;
+  }
+  return mergeSort(0,nums.length-1)
+};
