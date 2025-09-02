309951f27b423d190d7a2b32d3db7a2bd8561874
Shubham28497 <shubhamgupta24640020@gmail.com>
2025-09-02 09:40:53 +0530

    add logic

--- Step3-Solve-problems-on-array/Lec2-Medium/next_permutation.js
+++ Step3-Solve-problems-on-array/Lec2-Medium/next_permutation.js
@@ -35,3 +35,25 @@
 
 // Step4: Reverse the suffix starting from i + 1
 // Because after index i, the sequence is in descending order, reversing makes it ascending (smallest possible).
+var nextPermutation = function(nums) {
+    let n=nums.length;
+    let i=n-2;
+    while(i>=0 && nums[i]>=nums[i+1]){
+        i--
+    }
+    if(i>=0){
+        let j=n-1;
+        while(nums[j]<=nums[i]){
+            j--
+        }
+        [nums[i],nums[j]]=[nums[j],nums[i]];
+    }
+    reverse(nums,i+1,n-1)
+};
+function reverse(nums,start,end){
+    while(start<end){
+        [nums[start],nums[end]]=[nums[end],nums[start]]
+        start++
+        end--
+    }
+}
